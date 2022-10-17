544f394a83b3341a2592637a36181a89902ee10e
Sofiia <voznyakson@gmail.com>
2022-10-17 18:43:46 +0300

    __init__.py file and views.py file added. Application runs correctly

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from backEnd_1 import views
---
+++ views.py
@@ -0,0 +1,22 @@
+from flask import jsonify, request
+from backEnd_1 import app
+
+CATEGORIES = [
+    {
+        "id": 1,
+        "name": "Food",
+    },
+]
+
+# GET /categories
+#POST /category
+
+@app.route("/categories")
+def get_categories():
+    return jsonify({"categories": CATEGORIES})
+
+@app.route("/category", methods=['POST'])
+def create_category():
+    request_data = request.get_json()
+    CATEGORIES.append(request_data)
+    return jsonify({"status": "ok"})
